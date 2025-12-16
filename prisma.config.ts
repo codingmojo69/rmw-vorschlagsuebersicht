449ff70aaefa1e745e8dfa036c67082db2d2da66
Alexander Tschmel <alexander.tschmel@rmw-wohnmoebel.de>
2025-12-16 11:37:44 +0100

    Enable driverAdapters and fix datasource url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  // Der Compiler verlangt hier "datasource" (Einzahl)
   datasource: {
-    // Hier übergeben wir die URL für die Datenbank-Verbindung
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
+    // Das '|| ""' am Ende garantiert TypeScript, dass es immer ein String ist.
+    url: process.env.DIRECT_URL || process.env.DATABASE_URL || "",
   },
 });
